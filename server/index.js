91af54d85ee4b75901de293c39d14a09e8401309
TaliV <tali.vulf@gmail.com>
2021-07-06 18:43:47 +0300

    Move auth endpoints to auth folder

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const { Op, sequelize, Customer, Category, Product, ShoppingCart, CartItem, Order } = require('./models');
+const authRoute = require('./routes/auth');
 
 
 app.use(cors());
@@ -10,78 +11,7 @@ app.use(express.urlencoded({
 }));
 app.use(express.json());
 
-
-const validateEmail = (email) => {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
-
-
-//register step 1
-app.post('/registration-check', async (req, res) => {
-    const { idNumber, email, password, confirmPassword } = req.body;
-
-    let registeredIdNumber;
-
-    if (idNumber) {
-        registeredIdNumber = await Customer.findOne({ where: { id: idNumber } })
-    }
-
-    if (!idNumber || !email || !password || !confirmPassword) {
-        res.send({ error: 'נא למלא את כל השדות' })
-    } else if (!validateEmail(email)) {
-        res.send({ error: 'כתובת אימייל אינה תקינה' })
-    } else if (registeredIdNumber) {
-        res.send({ error: 'תעודת זהות קיימת במערכת' })
-    } else if (password && password.length < 6) {
-        res.send({ error: 'אורך סיסמה צריך להיות לפחות שישה תווים' })
-    } else if (password !== confirmPassword) {
-        res.send({ error: 'סיסמאות לא תואמות' })
-    } else {
-        res.send()
-    }
-})
-
-//register step 2
-app.post('/register', async (req, res) => {
-    const { idNumber, email, password, firstName, lastName, city, street } = req.body;
-    if (!firstName || !lastName || !city || !street) {
-        res.send({ error: 'נא למלא את כל השדות' })
-    } else {
-        try {
-            await sequelize.sync();
-            await Customer.create({
-                id: idNumber,
-                first_name: firstName,
-                last_name: lastName,
-                email,
-                password,
-                city,
-                street
-            });
-            res.send();
-        } catch (e) {
-            console.error(e)
-            res.send(e)
-        }
-    }
-})
-
-//login
-app.post('/login', async (req, res) => {
-    const { idNumber, password } = req.body;
-
-    const loggingInUser = await Customer.findOne({ where: { id: idNumber } });
-    console.log(loggingInUser)
-
-    if (!loggingInUser) {
-        res.send({ error: 'משתמש לא נמצא' })
-    } else if (loggingInUser.password !== password) {
-        res.send({ error: 'סיסמה לא נכונה' })
-    } else {
-        res.send({ userId: loggingInUser.id });
-    }
-})
+app.use(authRoute)
 
 //get general store details 
 app.get('/details', async (req, res) => {
---
+++ server/routes/auth.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const router = express.Router();
+const { sequelize, Customer } = require('../models');
+
+
+const validateEmail = (email) => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+//register step 1
+router.post('/registration-check', async (req, res) => {
+    const { idNumber, email, password, confirmPassword } = req.body;
+
+    let registeredIdNumber;
+
+    if (idNumber) {
+        registeredIdNumber = await Customer.findOne({ where: { id: idNumber } })
+    }
+
+    if (!idNumber || !email || !password || !confirmPassword) {
+        res.send({ error: 'נא למלא את כל השדות' })
+    } else if (!validateEmail(email)) {
+        res.send({ error: 'כתובת אימייל אינה תקינה' })
+    } else if (registeredIdNumber) {
+        res.send({ error: 'תעודת זהות קיימת במערכת' })
+    } else if (password && password.length < 6) {
+        res.send({ error: 'אורך סיסמה צריך להיות לפחות שישה תווים' })
+    } else if (password !== confirmPassword) {
+        res.send({ error: 'סיסמאות לא תואמות' })
+    } else {
+        res.send()
+    }
+})
+
+//register step 2
+router.post('/register', async (req, res) => {
+    const { idNumber, email, password, firstName, lastName, city, street } = req.body;
+    if (!firstName || !lastName || !city || !street) {
+        res.send({ error: 'נא למלא את כל השדות' })
+    } else {
+        try {
+            await sequelize.sync();
+            await Customer.create({
+                id: idNumber,
+                first_name: firstName,
+                last_name: lastName,
+                email,
+                password,
+                city,
+                street
+            });
+            res.send();
+        } catch (e) {
+            console.error(e)
+            res.send(e)
+        }
+    }
+})
+
+//login
+router.post('/login', async (req, res) => {
+    const { idNumber, password } = req.body;
+
+    const loggingInUser = await Customer.findOne({ where: { id: idNumber } });
+    console.log(loggingInUser)
+
+    if (!loggingInUser) {
+        res.send({ error: 'משתמש לא נמצא' })
+    } else if (loggingInUser.password !== password) {
+        res.send({ error: 'סיסמה לא נכונה' })
+    } else {
+        res.send({ userId: loggingInUser.id });
+    }
+})
+
+module.exports = router;
