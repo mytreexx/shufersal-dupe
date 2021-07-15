027030a1b4663d882fb8d7dbae1d24cf934d44c2
TaliV <tali.vulf@gmail.com>
2021-07-15 17:20:06 +0300

    Add fetch requests for registrations in utils

--- client/src/utils.js
+++ client/src/utils.js
@@ -3,3 +3,11 @@ const serverUrl = process.env.REACT_APP_SERVER_URL || '';
 export const getStoreDetails = (requestOptions) => {
     return fetch(`${serverUrl}/details`, requestOptions)
 }
+
+export const checkForRegistration = (requestOptions) => {
+    return fetch(`${serverUrl}/register-step-one`, requestOptions)
+}
+
+export const completeRegistration = (requestOptions) => {
+    return fetch(`${serverUrl}/register-step-two`, requestOptions)
+}
