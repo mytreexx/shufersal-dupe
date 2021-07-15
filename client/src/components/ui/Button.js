dda0993d8753dc7cc2db5b532464f0c605b37de2
TaliV <tali.vulf@gmail.com>
2021-07-15 16:09:47 +0300

    Fix button component to fit small and light attributes

--- client/src/components/ui/Button.js
+++ client/src/components/ui/Button.js
@@ -4,17 +4,18 @@ const Button = styled.button`
     background-color: ${(props) => (props.light ? 'transparent' : '#D51C4A')};
     border: #D51C4A 1px solid;
     outline: none;
-    padding: 15px;
-    color: white;
+    padding: ${(props) => (props.small ? '10px' : '15px')};
+    color: ${(props) => (props.light ? '#D51C4A' : 'white')};
     border-radius: 4px;
-    font-weight: 700;
-    width: 200px;
-    font-size: 21px;
+    padding: ${(props) => (props.small ? 'normal' : 'bold')};
+    /* width: 200px; */
+    font-size: ${(props) => (props.small ? '16px' : '21px')};
     cursor: pointer;
     transition: .5s;
 
     :hover {
         background-color: ${(props) => (props.light ? '#D51C4A' : '#BB103B')};
+        color: white;
     }
 
     a {
