b36d7968f09c0e580459d299bb4a92daaea950a2
TaliV <tali.vulf@gmail.com>
2021-07-15 18:36:39 +0300

    Add login routing

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route, useHistory } from 'react-router-dom';
 import './App.css';
 import LandingPage from './components/pages/LandingPage';
 import Register from './components/pages/Register';
+import Login from './components/pages/Login';
 
 
 function App() {
@@ -14,6 +15,10 @@ function App() {
 
       <Route path="/register">
         <Register />
+      </Route>
+
+      <Route path="/login">
+        <Login />
       </Route>
     </Switch>
   );
