4da1a08ae808b24c6ae74d376d04f1e691561023
TaliV <tali.vulf@gmail.com>
2021-07-16 12:44:15 +0300

    add jwt to order/post endpoint

--- server/routes/order.js
+++ server/routes/order.js
@@ -24,38 +24,44 @@ router.get('/', verifyTokenOrError, (req, res) => {
 })
 
 // create order
-router.post('/', async (req, res) => {
-    const { customerId, city, street, shippingDate, creditCard } = req.body;
+router.post('/', verifyTokenOrError, (req, res) => {
+    const { city, street, shippingDate, creditCard } = req.body;
 
-    const currentCart = await ShoppingCart.findOne({
-        where: { customer_id: customerId },
-        order: [['id', 'DESC']],
+    jwt.verify(req.token, 'supersecretkey', async (err, authData) => {
+        if (err) {
+            res.sendStatus(403);
+        } else {
+            const currentCart = await ShoppingCart.findOne({
+                where: { customer_id: authData.loggingInUser.id },
+                order: [['id', 'DESC']],
+            });
+
+            const cartItems = await CartItem.findAll({ where: { cart_id: currentCart.id } })
+            let totalPrice = 0;
+            cartItems.forEach(item => totalPrice = totalPrice + item.total_price)
+
+            const date = new Date();
+            const formattedDate = date.toISOString().slice(0, 10).replace(/-/g, "-");
+
+            try {
+                await sequelize.sync();
+                const newOrder = await Order.create({
+                    customer_id: authData.loggingInUser.id,
+                    cart_id: currentCart.id,
+                    total_price: totalPrice,
+                    city,
+                    street,
+                    shipping_date: shippingDate,
+                    order_date: formattedDate,
+                    credit_card_last_digits: creditCard
+                });
+                res.send({ message: `order number ${newOrder.id} will be delivered at ${shippingDate}` });
+            } catch (e) {
+                console.error(e)
+                res.send(e)
+            }
+        }
     });
-
-    const cartItems = await CartItem.findAll({ where: { cart_id: currentCart.id } })
-    let totalPrice = 0;
-    cartItems.forEach(item => totalPrice = totalPrice + item.total_price)
-
-    const date = new Date();
-    const formattedDate = date.toISOString().slice(0, 10).replace(/-/g, "-");
-
-    try {
-        await sequelize.sync();
-        const newOrder = await Order.create({
-            customer_id: customerId,
-            cart_id: currentCart.id,
-            total_price: totalPrice,
-            city,
-            street,
-            shipping_date: shippingDate,
-            order_date: formattedDate,
-            credit_card_last_digits: creditCard
-        });
-        res.send({ message: `order number ${newOrder.id} will be delivered at ${shippingDate}` });
-    } catch (e) {
-        console.error(e)
-        res.send(e)
-    }
 })
 
 module.exports = router;
