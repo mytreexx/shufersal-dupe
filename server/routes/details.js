3730054ae88e59dc5341e7efbdfccff915b69a2e
TaliV <tali.vulf@gmail.com>
2021-07-16 12:52:05 +0300

    add jwt to product/search/get endpoint

--- server/routes/details.js
+++ server/routes/details.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { Customer, Product, ShoppingCart, Order } = require('../models');
+const jwt = require('jsonwebtoken');
+const verifyTokenOrGuest = require('../utils/middlewares/JWT/VerifyTokenOrGuest');
 
 
 //get general store details 
-router.post('/', async (req, res) => {
-    const { customerId } = req.body;
+router.post('/', verifyTokenOrGuest, async (req, res) => {
 
     const products = await Product.findAll();
     const orders = await Order.findAll();
@@ -13,39 +14,81 @@ router.post('/', async (req, res) => {
     let messageToUser = "ברוך הבא לשופרסל";
     let hasActiveCart = false;
 
-    if (customerId) {
-        const customer = await Customer.findOne({ where: { id: customerId } });
-        const cart = await ShoppingCart.findOne({
-            where: { customer_id: customerId },
-            order: [['id', 'DESC']],
-        });
+    jwt.verify(req.token, 'supersecretkey', async (err, authData) => {
+        if (authData) {
+            const customer = await Customer.findOne({ where: { id: authData.loggingInUser.id } });
+            const cart = await ShoppingCart.findOne({
+                where: { customer_id: customer.id },
+                order: [['id', 'DESC']],
+            });
 
-        if (cart) {
-            const order = await Order.findOne({ where: { cart_id: cart.id } })
-            if (order) {
-                messageToUser = `העגלה שלך ריקה, הזמנה אחרונה בוצעה ב-${order.order_date}`;
-            } else {
-                messageToUser = `מוזמנים להמשיך בתהליך הקניה מ-${cart.created_at}`;
-                hasActiveCart = true;
+            if (cart) {
+                const order = await Order.findOne({ where: { cart_id: cart.id } })
+                if (order) {
+                    messageToUser = `העגלה שלך ריקה, הזמנה אחרונה בוצעה ב-${order.order_date}`;
+                } else {
+                    messageToUser = `מוזמנים להמשיך בתהליך הקניה מ-${cart.created_at}`;
+                    hasActiveCart = true;
+                }
             }
+            res.send({
+                numberOfProducts: products.length,
+                numberOfOrders: orders.length,
+                messageToUser,
+                hasActiveCart,
+                customerName: customer.first_name
+            })
         }
-        res.send({
-            numberOfProducts: products.length,
-            numberOfOrders: orders.length,
-            messageToUser,
-            hasActiveCart,
-            customerName: customer.first_name
-        })
-    }
-    else {
-        res.send({
-            numberOfProducts: products.length,
-            numberOfOrders: orders.length,
-            messageToUser,
-            hasActiveCart,
-            customerName: "אורח"
-        })
-    }
+        else {
+            res.send({
+                numberOfProducts: products.length,
+                numberOfOrders: orders.length,
+                messageToUser,
+                hasActiveCart,
+                customerName: "אורח"
+            })
+        }
+    });
+
+    // const products = await Product.findAll();
+    // const orders = await Order.findAll();
+
+    // let messageToUser = "ברוך הבא לשופרסל";
+    // let hasActiveCart = false;
+
+    // if (customerId) {
+    //     const customer = await Customer.findOne({ where: { id: customerId } });
+    //     const cart = await ShoppingCart.findOne({
+    //         where: { customer_id: customerId },
+    //         order: [['id', 'DESC']],
+    //     });
+
+    //     if (cart) {
+    //         const order = await Order.findOne({ where: { cart_id: cart.id } })
+    //         if (order) {
+    //             messageToUser = `העגלה שלך ריקה, הזמנה אחרונה בוצעה ב-${order.order_date}`;
+    //         } else {
+    //             messageToUser = `מוזמנים להמשיך בתהליך הקניה מ-${cart.created_at}`;
+    //             hasActiveCart = true;
+    //         }
+    //     }
+    //     res.send({
+    //         numberOfProducts: products.length,
+    //         numberOfOrders: orders.length,
+    //         messageToUser,
+    //         hasActiveCart,
+    //         customerName: customer.first_name
+    //     })
+    // }
+    // else {
+    //     res.send({
+    //         numberOfProducts: products.length,
+    //         numberOfOrders: orders.length,
+    //         messageToUser,
+    //         hasActiveCart,
+    //         customerName: "אורח"
+    //     })
+    // }
 })
 
 module.exports = router;
