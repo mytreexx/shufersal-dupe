3a6b99a88ca3e9539145dddf95691b1201697e17
TaliV <tali.vulf@gmail.com>
2021-07-16 17:35:47 +0300

    Get all products and all categories in store component

--- client/src/components/pages/Store.js
+++ client/src/components/pages/Store.js
@@ -1,7 +1,75 @@
-const Store = () => {
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+import SecondaryNavbar from '../ui/SecondaryNavbar';
+import Logo from '../../assets/Shufersal-logo-large.png';
+import { getCategoriesFromServer, getAllProducts } from '../../utils';
+
+
+const Store = ({ currentUser }) => {
+    const [categories, setCategories] = useState([]);
+    const [products, setProducts] = useState();
+
+    useEffect(() => {
+        getCategoriesFromServer(currentUser)
+            .then(response => response.json())
+            .then(data => {
+                setCategories(data)
+            })
+    }, []);
+
+
+    useEffect(() => {
+        getAllProducts(currentUser)
+            .then(response => response.json())
+            .then(data => {
+                setProducts(data)
+            })
+    }, []);
+
     return (
-        <div>store</div>
+        <>
+            <NavContainer>
+                <img src={Logo} />
+                <SecondaryNavbar />
+            </NavContainer>
+
+            <CategoriesNav>
+                {categories && categories.map(category => <div>{category.category}</div>)}
+            </CategoriesNav>
+
+            {products && products.map(product => <div>{product.product_name}</div>)}
+        </>
     )
 }
 
 export default Store;
+
+const NavContainer = styled.div`
+    display: flex;
+    height: 110px;
+    align-items: center;
+    border-bottom: 1px #f0f3f9 solid;
+    padding-right: 40px;
+`;
+
+const CategoriesNav = styled.div`
+    display: flex; 
+    padding: 20px;
+    padding-right: 50px;
+
+    div {
+        font-size: 16px;
+        margin: 10px 0 10px 10px;
+        padding: 5px 0 5px 10px;
+        border-left: 2px solid #e0e2e9;
+
+        :hover {
+            color: #048BF3
+        }
+    }
+
+    div:last-child {
+        border-left: none;
+    }
+`;
