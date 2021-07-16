df2e555ee9be3c455a5588b78c9fc7c09f5a31c4
TaliV <tali.vulf@gmail.com>
2021-07-16 20:20:52 +0300

    Update button component to support more sizes

--- client/src/components/ui/Button.js
+++ client/src/components/ui/Button.js
@@ -4,12 +4,11 @@ const Button = styled.button`
     background-color: ${(props) => (props.light ? 'transparent' : '#D51C4A')};
     border: #D51C4A 1px solid;
     outline: none;
-    padding: ${(props) => (props.small ? '10px' : '15px')};
+    padding: ${(props) => (props.small ? '10px' : props.mini ? '5px 10px' : '15px')};
     color: ${(props) => (props.light ? '#D51C4A' : 'white')};
     border-radius: 4px;
     padding: ${(props) => (props.small ? 'normal' : 'bold')};
-    /* width: 200px; */
-    font-size: ${(props) => (props.small ? '16px' : '21px')};
+    font-size: ${(props) => ((props.small || props.mini) ? '16px' : '21px')};
     cursor: pointer;
     transition: .5s;
 
