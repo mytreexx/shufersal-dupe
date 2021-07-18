b27cba715abbb55d648fe2080bb28e8e8c9a945c
TaliV <tali.vulf@gmail.com>
2021-07-18 23:21:40 +0300

    fix /api urls

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,4 +1,4 @@
-const serverUrl = process.env.REACT_APP_SERVER_URL || 'api/';
+const serverUrl = process.env.REACT_APP_SERVER_URL || 'api';
 
 export const getStoreDetails = (currentUser) => {
     const requestOptions = {
