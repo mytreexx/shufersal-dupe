82ff25e968ec3c1bc8d5bbd22f368b498fd7b707
TaliV <tali.vulf@gmail.com>
2021-07-18 12:04:09 +0300

    Style order page

--- client/src/components/pages/OrderPage.js
+++ client/src/components/pages/OrderPage.js
@@ -28,6 +28,7 @@ const OrderPage = ({ currentUser }) => {
         const regex = /^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/
         return regex.test(cardNumber);
     }
+
     useEffect(() => {
         getCartItems(currentUser)
             .then(response => response.json())
@@ -49,57 +50,61 @@ const OrderPage = ({ currentUser }) => {
     const makeOrder = (e) => {
         e.preventDefault();
         if (validateCreditCard(creditCard)) {
-        onMakeOrder(currentUser, city, street, shippingDate, creditCard)
-            .then((response) => {
-                if (response.ok) {
-                    response.json()
-                        .then((response) => toast.success(response.message))
-                    setTimeout(() => {
-                        history.push('/store');
-                    }, 3000);
-                } else {
-                    response.json()
-                        .then((response) => toast.error(response.error));
-                }
-            });
+            onMakeOrder(currentUser, city, street, shippingDate, creditCard)
+                .then((response) => {
+                    if (response.ok) {
+                        response.json()
+                        history.push({
+                            pathname: '/receipt',
+                            state: shippingDate
+                        });
+                    } else {
+                        response.json()
+                            .then((response) => toast.error(response.error));
+                    }
+                });
         } else {
             toast.error("אמצעי תשלום אינו תקין")
-    }
+        }
     }
 
     return (
-        <>
-            <NavContainer>
-                <img src={Logo} />
-                <SecondaryNavbar />
-            </NavContainer>
+        <Container>
+            <div>
+                <NavContainer>
+                    <img src={Logo} />
+                    <SecondaryNavbar />
+                </NavContainer>
 
-            <Cart cartItems={cartItems} />
-            <OrderDetails onSubmit={makeOrder}>
-                <ToastContainer
-                    position="top-center"
-                    autoClose={false}
-                    newestOnTop={false}
-                    closeOnClick
-                    rtl={false}
-                    pauseOnFocusLoss
-                    draggable
-                />
-                <Input
-                    required
-                    label="עיר מגורים"
-                    type="text"
-                    value={city}
-                    onChange={(e) => setCity(e.target.value)}
-                />
+                <Header>
+                    ביצוע הזמנה
+                </Header>
 
-                <Input
-                    required
-                    label="רחוב"
-                    type="text"
-                    value={street}
-                    onChange={(e) => setStreet(e.target.value)}
-                />
+                <OrderForm onSubmit={makeOrder}>
+                    <ToastContainer
+                        position="bottom-right"
+                        autoClose={false}
+                        newestOnTop={false}
+                        closeOnClick
+                        rtl={false}
+                        pauseOnFocusLoss
+                        draggable
+                    />
+                    <Input
+                        required
+                        label="עיר מגורים"
+                        type="text"
+                        value={city}
+                        onChange={(e) => setCity(e.target.value)}
+                    />
+
+                    <Input
+                        required
+                        label="רחוב"
+                        type="text"
+                        value={street}
+                        onChange={(e) => setStreet(e.target.value)}
+                    />
 
                     <Label>
                         תאריך למשלוח
@@ -110,18 +115,23 @@ const OrderPage = ({ currentUser }) => {
                         </Select>
                     </Label>
 
-                <Input
-                    required
-                    label="אמצעי תשלום"
+                    <Input
+                        required
+                        label="אמצעי תשלום"
                         type="number"
-                    value={creditCard}
-                    onChange={(e) => setCreditCard(e.target.value)}
-                />
-                <Button type="submit">בצע הזמנה כעת</Button>
-            </OrderDetails>
-            <Link to="/store"><Button small > חזור</Button></Link>
+                        value={creditCard}
+                        onChange={(e) => setCreditCard(e.target.value)}
+                    />
 
-        </>
+                    <Button type="submit" small>בצע הזמנה כעת</Button>
+                    <Link to="/store"><Button small light> חזור</Button></Link>
+                </OrderForm>
+            </div>
+            <DisabledElement>
+                <Cart cartItems={cartItems} />
+            </DisabledElement>
+
+        </Container>
     )
 }
 
@@ -135,5 +145,59 @@ const NavContainer = styled.div`
     padding-right: 40px;
 `;
 
-const OrderDetails = styled.form`
+const OrderForm = styled.form`
+    margin: 80px;
+
+    > * {
+        margin-bottom: 20px;
+    }
+
+    button {
+        margin-left: 10px;
+    }
+
+    
+    .Toastify__toast {
+        background-color: #D51C4A;
+    }
 `;
+
+const Container = styled.div`
+    display: flex;
+`;
+
+const DisabledElement = styled.div`
+    pointer-events: none;
+    filter: grayscale(1) brightness(1.1);
+`;
+
+const Header = styled.div`
+    display: flex;
+    align-items: center;
+    padding-right: 50px;
+    background-color: #D51C4A;
+    height: 60px;
+    color: white;
+    font-size: 20px;
+    margin-bottom: 20px;
+`;
+
+const Select = styled.select`
+    width: 250px;
+    height: 40px;
+    border: 1px transparent solid;
+    outline: none;
+    border-bottom: 1px rgb(47 65 110) solid;
+    font-size: 16px;
+    padding-right: 5px;
+    margin-top: 10px;
+`;
+
+const Label = styled.label`
+    display: flex;
+    flex-direction: column;
+    font-weight: bold;
+    font-size: 14px;
+`;
+
+
